f972faf1f5f8ae072c02b77c08857c9a4216998c
Ivan Horpynych-Raduzhenko <Ivan.Horpynych-Raduzhenko@externals.adidas.com>
2022-02-09 23:21:58 +0200

    Some fix

---
+++ src/test/java/com/task/demo/controller/UserControllerUnitTest.java
@@ -0,0 +1,50 @@
+package com.task.demo.controller;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.task.demo.model.dto.UserDto;
+import com.task.demo.model.entity.User;
+import com.task.demo.service.UserService;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(controllers = UserController.class)
+class UserControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    UserService userService;
+
+    User user2 = new User(2l, 1l, 2l, 1l);
+    User user3 = new User(3l, 2l, 2l, 3l);
+
+    @Test
+    void getTopResultOfUser() throws Exception {
+        UserDto user1 = new UserDto();
+        user1.setUserId(1l);
+        user1.setLevelId(1l);
+        user1.setResult(1l);
+        when(userService.findTop20ByUserId(user1.getUserId())).thenReturn(List.of(user1));
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/user/userinfo/1").contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()).
+                andExpect(jsonPath("$.*", hasSize(2))).
+                andExpect((ResultMatcher) jsonPath("$[0].user_id", is("1"))).
+                andExpect(jsonPath("$[0].did", anything())).
+                andExpect((ResultMatcher) jsonPath("$[1].user_id", is("1")));
+    }
+}
